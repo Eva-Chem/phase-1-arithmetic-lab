9334c8abfccf91037c2bbc7a1c972cf3330a4471
Eva Chemngorem <eva@Evas-MacBook-Air.local>
2025-07-10 18:03:21 +0300

    Complete arithmetic operations assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+// Multiply two numbers to get 62
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+// Generate a random number greater than 0
+let random = Math.floor(Math.random() * 100) + 1;
+
+// Modulo to get remainder 4
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;
+
+// Find the highest number in a set
+let max = Math.max(5, 10, 15, 20);
